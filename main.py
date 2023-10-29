ef598845beec4d30cb601d219cccc70d6fc0c182
half-real-SCRACX <robertlupas09@gmail.com>
2023-10-29 17:35:04 +0000

    Changed variable names

--- main.py
+++ main.py
@@ -3,17 +3,17 @@ import json
 from data_adder import process
 
 template_file_path = "./example-data/template.html"
-json_file = "./example-data/data.json"
-output_file = "./example-data/output.html"
+json_file_path = "./example-data/data.json"
+output_file_path = "./example-data/output.html"
 
-with open(json_file, 'r') as json_file:
+with open(json_file_path, 'r') as json_file:
   data = json.load(json_file)
   json_file.close()
 
-with open(template_file_path, 'r') as template:
-  template_contents = template.read()
-  template.close()
+with open(template_file_path, 'r') as template_file:
+  template_contents = template_file.read()
+  template_file.close()
 
-with open(output_file, 'w') as output_file:
+with open(output_file_path, 'w') as output_file:
   output_file.write(process(template_contents, data))
   output_file.close()
